d13c1268dd0eb248ebdc739bfb64b197a24feccf
gevorg180496 <gevorg180496@gmail.com>
2026-05-26 15:02:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -352,4 +352,7 @@ app.on_shutdown.append(on_shutdown)
 # =========================
 
 if __name__ == "__main__":
-    web.run_app(app, host="0.0.0.0", port=PORT)
+    try:
+        web.run_app(app, host="0.0.0.0", port=PORT)
+    except Exception as e:
+        print("START ERROR:", e)
